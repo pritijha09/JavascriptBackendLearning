fa900bafc65c9b38198a25f4aa5807b23bf12cba
Priti Kumari <priti.kumari@primussoft.com>
2023-12-13 16:16:18 +0530

    create hospital data models

---
+++ models/ecommerce/hospital-management/hospital.models.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const hospitalSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        requred: true,
+    },
+    addressLine1: {
+        type: String,
+        required: true
+    },
+    addressLine2: {
+        type: String,
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    pincode: {
+        type: String,
+        required: true
+    },
+    specializedIn: [{
+        type: String,
+    }]
+}, {timestamps: true})
+
+export const Hospital = mongoose.model("Hospital", hospitalSchema)
